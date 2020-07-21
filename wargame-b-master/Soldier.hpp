c9504ae99b737e039e13416b60a623058744e020
amir hoshen <mi.rosh160@gmail.com>
2020-07-21 20:00:04 +0300

    finished soldier header file. need to be tested.

--- wargame-b-master/Soldier.hpp
+++ wargame-b-master/Soldier.hpp
@@ -5,5 +5,21 @@
 #include <stdexcept>
 
 class Soldier{
+public:
+    enum Type{
+        FootSoldier,
+        Sniper,
+        Paramedic,
+    };
+    int _hp;
+    int _dpa;
+    int _team;
+    bool _isCommander;
+    Type _type;
 
+    Soldier(int hp, int dpa, int team, bool commander, Type type): _hp(hp), _dpa(dpa), _team(team), _isCommander(commander), _type(type){}
+    virtual ~Soldier(){};
+    virtual void attack(std::vector<std::vector<Soldier*>> &board, std::pair<int,int> source) =0;
+    virtual void specialAttack(std::vector<std::vector<Soldier*>> &board, std::pair<int, int> source) = 0;
+    virtual void heal() = 0;
 };
